e11c484027a9e32f9fb2e7c3c2b8ea3778a3a7b6
Elijah <elijah.blake@usu.edu>
2024-02-05 22:44:04 +0000

    Update URLs and views to server React

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
-from front_end.views import index, add_numbers
-from django.urls import path, include
+from front_end.views import serve_react, add_numbers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/add/', add_numbers, name='add_numbers'),
-    path('', include('front_end.urls')),
+    # Catch-all pattern for serving the React app
+    re_path(r"^(?P<path>.*)$", serve_react, {"document_root": settings.REACT_APP_BUILD_PATH}),
 ]
--- front_end/urls.py
+++
@@ -1,7 +0,0 @@
-# front_end/urls.py
-from django.urls import path
-from .views import index
-
-urlpatterns = [
-    path('', index, name='index'),
-]

9a0e119be8230c91043c08309e942944784eb868
Manodiestra <52931506+Manodiestra@users.noreply.github.com>
2024-03-31 18:54:02 -0600

    Added modifications to BE to account for changes in user requests to include tokens and user IDs.

--- front_end/views.py
+++ front_end/views.py
@@ -5,6 +5,7 @@ from django.http import Http404, HttpResponseServerError
 from django.utils._os import safe_join
 from django.views.static import serve as static_serve
 from rest_framework.response import Response
+from rest_framework.permissions import BasePermission
 from rest_framework import viewsets, status
 from django.utils.timezone import now
 
@@ -28,6 +29,35 @@ def serve_react(request, path='', document_root=None):
         return static_serve(request, "index.html", document_root=document_root)
 
 
+class TokenPresent(BasePermission):
+    # Allows access only if a token is present in the headers.
+    def has_permission(self, request, view):
+        return 'Authorization' in request.headers
+
 class JournalEntryViewSet(viewsets.ModelViewSet):
-    queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
+    # Use custom permission class or remove it entirely if not needed
+    permission_classes = [TokenPresent]
+
+    def get_queryset(self):
+        # Get the user ID from a custom header or part of the request
+        user_id = self.request.headers.get('X-User-ID')
+        return JournalEntry.objects.filter(user_id=user_id)
+
+    def perform_create(self, serializer):
+        # Get the user ID from the request data and save it with the journal entry
+        user_id = self.request.data.get('user_id')
+        serializer.save(user_id=user_id)
+
+    # No need to override `create` and `update` methods if they don't have specific logic other than calling `perform_create` and `perform_update`
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        # Ensure the instance user_id matches the one in the request data or headers
+        request_user_id = request.data.get('user_id') or request.headers.get('X-User-ID')
+        if instance.user_id != request_user_id:
+            return Response({"detail": "You do not have permission to update this entry."}, status=status.HTTP_403_FORBIDDEN)
+
+        return super(JournalEntryViewSet, self).update(request, *args, **kwargs)
